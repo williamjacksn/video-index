0a41ba1f53c82cf69a9a4c2c50bedf3b4eed547c
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-06-05 15:50:42 -0500

    Gen yaml

---
+++ ci/gen-dependabot.py
@@ -0,0 +1,20 @@
+import pathlib
+import gen
+
+
+def update(ecosystem: str) -> dict:
+    return {
+        "package-ecosystem": ecosystem,
+        "allow": [{"dependency-type": "all"}],
+        "directory": "/",
+        "schedule": {"interval": "daily"},
+    }
+
+
+ecosystems = ["docker", "github-actions", "npm", "uv"]
+content = {
+    "version": 2,
+    "updates": [update(e) for e in ecosystems],
+}
+
+gen.gen(content, pathlib.Path(".github/dependabot.yaml"))
