d97c8cc73b5efca5b23d4f1f42a88aef988f5cf3
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-07-02 14:15:22 -0500

    Simplify pathlib.Path() call in versions.py

--- video_index/versions.py
+++ video_index/versions.py
@@ -1,7 +1,7 @@
 import json
 import pathlib
 
-package_json = pathlib.Path().resolve() / "package.json"
+package_json = pathlib.Path("package.json")
 with package_json.open() as f:
     data = json.load(f)
 
